6bf37e22fb9ffc09189b7d043b7f42773aec0229
Shelley Arnold <shelle.kay@gmail.com>
2019-04-30 14:31:11 -0500

    printed it to the DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,8 @@
 const outEl = document.querySelector("#output");
 
 //add a heading that does not change
-outEl.innerHTML = "<h1>Active Businesses</h1>";
+outEl.innerHTML = `<h1>Active Businesses</h1> 
+    <h2>Big Spenders</h2>`;
 
 
 //forEach()
@@ -42,10 +43,55 @@ outEl.innerHTML = "<h1>Active Businesses</h1>";
 
 
 //join() makes a string
-function showBusiness(business) {
-  return `<div>${business.companyName}</div>`;
+// function showBusiness(business) {
+//   return `<div>${business.companyName}</div>`;
+// }
+
+// let newStuff = businesses.map(showBusiness).join("");
+// outEl.innerHTML += newStuff;
+
+
+// businesses
+//   .filter(biz => biz.orders >= 9000)
+//   .forEach(business => {
+//     outEl.innerHTML += `
+//         <h2>${business.companyName}</h2>
+//         <section>
+//         ${business.orders}
+//         </section>
+//         `;
+//     outEl.innerHTML += "<hr>";
+//   });
+
+//one way to get a company who makes an order over 9000
+function orderGreaterThan(value) {
+  if (value > 9000) {
+    return true;
+  }
+}
+const bigSpenders = businesses.filter(business => {
+  let businessSpender = false;
+  business.orders.forEach(order => {
+    if (orderGreaterThan(order)) {
+      businessSpender = true;
+    }
+  });
+  return businessSpender;
+});
+
+
+//another way to get a company who makes an order over 9000
+function checkDollar(order) {
+  return order > 9000;
 }
 
-let newStuff = businesses.map(showBusiness);
-outEl.innerHTML += newStuff.join(`<hr>`);
+const biggerSpenders = businesses.filter(business => {
+  return business.orders.find(checkDollar);
+});
 
+console.log("these are the bigger spenders", biggerSpenders)
+
+//print this to the dom
+for (i = 0; i < biggerSpenders.length; i ++) {
+    outEl.innerHTML += (biggerSpenders[i].companyName) += `<hr>`;
+}
